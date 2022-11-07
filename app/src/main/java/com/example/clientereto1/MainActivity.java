7f69fc19a5f7b9c84a22bda7f7f134c164234764
Estíbaliz Martín <estibalizmares@gmail.com>
2022-11-07 19:04:50 +0100

    Create layouts for Community and Favorites

--- app/src/main/java/com/example/clientereto1/MainActivity.java
+++ app/src/main/java/com/example/clientereto1/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.clientereto1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button signInBtn, registerBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         getSupportActionBar().hide();
+
+        signInBtn = findViewById(R.id.signInButtonHome);
+        registerBtn = findViewById(R.id.registerButtonHome);
+
+        signInBtn.setOnClickListener(view -> {
+            setContentView(R.layout.layout_sign_in);
+        });
+
+        registerBtn.setOnClickListener(view -> {
+            setContentView(R.layout.layout_register);
+        });
     }
 }
