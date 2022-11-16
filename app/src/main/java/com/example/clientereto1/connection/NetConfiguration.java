7e688699acd4bf5d54b7773013802b75ce05e222
DavidPereiroAmez <davidpereiroamez@gmail.com>
2022-11-16 15:32:17 +0100

    Resolve conflicts

--- app/src/main/java/com/example/clientereto1/connection/NetConfiguration.java
+++
@@ -1,7 +0,0 @@
-package com.example.clientereto1.connection;
-
-public class NetConfiguration {
-    //protected static final String USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36";
-    //protected final String theBaseUrl = "http://127.0.0.1:8090/api";
-    protected final String theBaseUrl = "http://10.5.7.27:8090/api";
-}
